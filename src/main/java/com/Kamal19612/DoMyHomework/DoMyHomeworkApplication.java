00d70d9ddcefdb065b2449662a5ea7968630f985
Kamal19612 <ouedraogokamalelise@gmail.com>
2025-07-28 09:20:58 +0000

    initialisation du projet

---
+++ src/main/java/com/Kamal19612/DoMyHomework/DoMyHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.Kamal19612.DoMyHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoMyHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoMyHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kamal19612/DoMyHomework/DoMyHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kamal19612.DoMyHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoMyHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
